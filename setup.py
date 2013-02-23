39f1d572a1fb82da165f68647806836dd6e406bb
Chris Laskey <contact@chrislaskey.com>
2013-02-23 13:30:45 -0500

    Another update for new WSGI settings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from lib.environment import Environment
-Environment().add_virtualenv_site_packages_to_path()
+Environment().add_virtualenv_site_packages_to_path(__file__)
 
 from lib.weddingphotoalbum import WeddingPhotoAlbumGenerator
 WeddingPhotoAlbumGenerator().create()
