8d567c04e77c88edc7e05b02f15b09ff282ac321
Chris Laskey <contact@chrislaskey.com>
2013-02-23 11:43:06 -0500

    Refactored separate environment classes back into main.py

--- main.py
+++ main.py
@@ -3,16 +3,12 @@
 from lib.environment import Environment
 Environment().add_virtualenv_site_packages_to_path()
 
-# from flask import Flask, abort, request, redirect, url_for, render_template, g
 from flask import Flask, g, render_template, request
-from config.appsetup import AppSetup
 from lib.requestparser import PageRequestParser, AdminPageRequestParser
 from lib.templateparser import TemplateVariableParser
 from lib.weddingphotoalbum import *
 
 app = Flask(__name__)
-
-# Routing
 
 @app.route('/<lang>/ceremony-and-reception/')
 def ceremony_and_reception(lang):
@@ -68,21 +64,10 @@ def homepage():
     common_page_processing()
     return render_template('site/homepage.html', **g.templatevars)
 
-# Admin Routing functions
-
-@app.route('/admin/')
-def admin_homepage():
-    common_admin_page_processing()
-    return 'admin'
-
-# Routing errors
-
 @app.errorhandler(404)
 def not_found(error):
     common_page_processing()
     return render_template('errors/404.html', **g.templatevars), 404
-
-# Helpers
 
 def common_page_processing():
     g.requestvars = return_page_requestvars()
@@ -102,16 +87,6 @@ def common_admin_page_processing():
 def set_admin_page_requestvars():
     g.requestvars = AdminPageRequestParser(request).return_requestvars()
 
-
-def set_jinja_config(app):
-    # Use a single % in templates
-    app.jinja_env.line_statement_prefix = '%'
-
 if __name__ == '__main__':
-    set_jinja_config(app);
-    app_config = {
-        'debug_mode_on_hostnames': ('laskey.local', 'cnsmac3.bu.edu')
-    }
-    app_setup = AppSetup(app_config).return_setup()
     app.jinja_env.line_statement_prefix = '%'
-    app.run(**app_setup)
+    app.run(debug=True)
