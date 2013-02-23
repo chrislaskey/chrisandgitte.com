9d6bb0ecc9ed82209da29e50d739c15b2971ca17
Chris Laskey <contact@chrislaskey.com>
2013-02-23 10:50:35 -0500

    Cleaned up /static files.

--- static/js/common.js
+++ static/js/common.js
@@ -1,12 +1,13 @@
 //Set global variables
 //Using global objects as pseudo-namespaces
-var table = {};
+var table = {},
+	anchors = {};
 
 //Initializing function
 $(function(){
 
 	//Binding functions
-	mobile.bind_responsive_toggle();
+	anchors.bind();
 
 });
 
@@ -14,4 +15,13 @@ $(function(){
 table.even_odd = function(){
 	$('table tr:odd').addClass('odd');
 	$('table tr:even').addClass('even');
+}
+
+// Anchor Methods
+anchors.bind = function(){
+	$('a[href="#"]').bind('click', function(){ return false; });
+	$('a.external, a[rel="external"]').each(function(e){
+		var title = ($(this).attr('title')) ?  $(this).attr('title') : 'This link will take you to an external website in a new window';
+		$(this).attr('target', '_blank').attr('title', title);
+	});
 }
