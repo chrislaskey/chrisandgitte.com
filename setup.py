1965469579568b9d062f89a6a436a74d86e1c89a
Chris Laskey <contact@chrislaskey.com>
2014-02-02 14:41:43 -0500

    Updated setup.py file to use new python binary path

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-
-from lib.environment import Environment
-Environment().add_virtualenv_site_packages_to_path(__file__)
+#!/usr/lib/virtualenvs/chrisandgitte/bin/python
 
 from application.weddingphotoalbum import WeddingPhotoAlbumGenerator
 WeddingPhotoAlbumGenerator().create()
